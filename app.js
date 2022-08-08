238661e5b5a641bf33c1f1d305d30b29d23e8d5f
Billy Montolalu <billy.montolalu@gmail.com>
2022-08-08 14:33:31 +0700

    add create code

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const readXlsxFile = require('read-excel-file/node')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 readXlsxFile('lab.xlsx').then((rows) => {
-    var nama = rows[22][0]
+    const index = 22
+    var nama = rows[index][0]
+    var satuan = rows[index][4]
+    var min = rows[index][5]
+    var max = rows[index][6]
+    prisma.nilai_normal.create({
+        data: {
+            name: nama,
+            min: min,
+            max: max,
+            satuan: satuan,
+            groupPemeriksaan: 7
+        }
+    })
 })
