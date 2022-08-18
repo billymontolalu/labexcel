89bae5f198a548c8c029df3b89acac49b74462c0
Billy Montolalu <billy.montolalu@gmail.com>
2022-08-18 11:41:55 +0700

    add nilai normal only

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ readXlsxFile('lab.xlsx').then((rows) => {
     const index = 22
     var nama = rows[index][0]
     var satuan = rows[index][4]
-    var min = rows[index][5]
-    var max = rows[index][6]
+    //var min = rows[index][5]
+    //var max = rows[index][6]
+    var nilai_normal = rows[index][3]
     prisma.nilai_normal.create({
         data: {
             name: nama,
-            min: min,
-            max: max,
+            nilai_normal: nilai_normal,
+            //min: min,
+            //max: max,
             satuan: satuan,
             groupPemeriksaan: 7
         }
